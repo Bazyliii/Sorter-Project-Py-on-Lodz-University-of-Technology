a034f98775edb8a99484ff6b65270bb017122a6e
Jarosław Wierzbowski <jaroslawierzbowski2001@gmail.com>
2024-06-06 22:33:13 +0200

    py.py vision template need a bit of refactioring but I added webserver with camera preview.

--- hsv_mask_maker.py
+++ hsv_mask_maker.py
@@ -12,21 +12,22 @@ def none(x):
     pass
 
 
-cv2.namedWindow("Up")
-cv2.namedWindow("Down")
-cv2.createTrackbar("Hue", "Up", 0, 180, none)
-cv2.createTrackbar("Saturation", "Up", 0, 255, none)
-cv2.createTrackbar("Value", "Up", 0, 255, none)
-cv2.createTrackbar("Hue", "Down", 0, 180, none)
-cv2.createTrackbar("Saturation", "Down", 0, 255, none)
-cv2.createTrackbar("Value", "Down", 0, 255, none)
+cv2.namedWindow("Hue", flags=cv2.WINDOW_AUTOSIZE)
+cv2.createTrackbar("HueLower", "Hue", 0, 180, none)
+cv2.createTrackbar("SatLower", "Hue", 0, 255, none)
+cv2.createTrackbar("ValueLower", "Hue", 0, 255, none)
+cv2.createTrackbar("HueUpper", "Hue", 180, 180, none)
+cv2.createTrackbar("SatUpper", "Hue", 255, 255, none)
+cv2.createTrackbar("ValueUpper", "Hue", 255, 255, none)
 
 
 while True:
     _, frame = cap.read()
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv_frame, (cv2.getTrackbarPos("Hue", "Down"), cv2.getTrackbarPos("Saturation", "Down"), cv2.getTrackbarPos("Value", "Down")), (cv2.getTrackbarPos("Hue", "Up"), cv2.getTrackbarPos("Saturation", "Up"), cv2.getTrackbarPos("Value", "Up")))
+    lower = numpy.array((cv2.getTrackbarPos("HueLower", "Hue"), cv2.getTrackbarPos("SatLower", "Hue"), cv2.getTrackbarPos("ValueLower", "Hue")))
+    upper = numpy.array((cv2.getTrackbarPos("HueUpper", "Hue"), cv2.getTrackbarPos("SatUpper", "Hue"), cv2.getTrackbarPos("ValueUpper", "Hue")))
+    mask = cv2.inRange(hsv_frame, lower, upper)
     cv2.imshow("Frame", frame)
-    cv2.imshow("Mask", mask)
+    cv2.imshow("Hue", mask)
     if cv2.waitKey(delay) & 0xFF == ord("q"):
         break
--- py.py
+++ py.py
@@ -1,6 +1,13 @@
 import cv2
 import numpy
 from enum import Enum
+from flask import Response
+from flask import Flask
+from flask import render_template
+from threading import Thread, Lock
+
+
+app = Flask(__name__)
 
 
 class Colors(Enum):
@@ -34,67 +41,105 @@ class HSVColorMask:
 area: tuple = (300, 500, 100, 340)
 radius: int = 11
 
-red_mask: HSVColorMask = HSVColorMask((0, 70, 70), (10, 255, 255)) # Git
-green_mask: HSVColorMask = HSVColorMask((45, 85, 53), (84, 255, 255)) # Git
-orange_mask: HSVColorMask = HSVColorMask((10, 50, 50), (45, 255, 255)) # Git
-white_mask: HSVColorMask = HSVColorMask((0, 0, 34), (180, 48, 255)) # Git
+red_mask: HSVColorMask = HSVColorMask((0, 70, 70), (10, 255, 255))  # Git
+green_mask: HSVColorMask = HSVColorMask((45, 85, 53), (84, 255, 255))  # Git
+orange_mask: HSVColorMask = HSVColorMask((10, 50, 50), (45, 255, 255))  # Git
+white_mask: HSVColorMask = HSVColorMask((0, 0, 34), (180, 48, 255))  # Git
 
-capture: cv2.VideoCapture = cv2.VideoCapture(0)
+capture: cv2.VideoCapture = cv2.VideoCapture(r"C:\Users\jaros\Downloads\recordd.avi")
 capture.set(cv2.CAP_PROP_FPS, 30)
 capture.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
 capture.set(cv2.CAP_PROP_EXPOSURE, 80)
 delay: int = int(1000 / capture.get(cv2.CAP_PROP_FPS))
 
-queue_of_elements: list = []
-tracker_types: dict[Colors, list] = {
-    color: [] for color in Colors
-}
+outputFrame = None
+lock = Lock()
 
 
-def detector(contours, color: Colors):
-    current_contours: list = []
-    for contour in contours:
-        if cv2.contourArea(contour) > 500:
-            moments: dict = cv2.moments(contour)
-            if moments["m00"] != 0:
-                center_x: int = int(moments["m10"] / moments["m00"])
-                center_y: int = int(moments["m01"] / moments["m00"])
+def cv():
+    global lock, outputFrame
+    queue_of_elements: list = []
+    tracker_types: dict[Colors, list] = {color: [] for color in Colors}
+
+    def detector(contours, color: Colors):
+        current_contours: list = []
+        for contour in contours:
+            if cv2.contourArea(contour) > 500:
+                moments: dict = cv2.moments(contour)
+                if moments["m00"] != 0:
+                    center_x: int = int(moments["m10"] / moments["m00"])
+                    center_y: int = int(moments["m01"] / moments["m00"])
+                    if (
+                        center_x > area[0]
+                        and center_x < area[1]
+                        and center_y > area[2]
+                        and center_y < area[3]
+                    ):
+                        cv2.circle(
+                            frame, (center_x, center_y), radius, color.get_rgb(), -1
+                        )
+                        current_contours.append((center_x, center_y))
+        for center_x, center_y in current_contours:
+            is_new: bool = True
+            for tracker_x, tracker_y in tracker_types[color]:
                 if (
-                    center_x > area[0]
-                    and center_x < area[1]
-                    and center_y > area[2]
-                    and center_y < area[3]
+                    numpy.linalg.norm(
+                        numpy.array([center_x, center_y])
+                        - numpy.array([tracker_x, tracker_y])
+                    )
+                    < 25
                 ):
-                    cv2.circle(frame, (center_x, center_y), radius, color.get_rgb(), -1)
-                    current_contours.append((center_x, center_y))
-    for center_x, center_y in current_contours:
-        is_new: bool = True
-        for tracker_x, tracker_y in tracker_types[color]:
-            if (
-                numpy.linalg.norm(
-                    numpy.array([center_x, center_y])
-                    - numpy.array([tracker_x, tracker_y])
-                )
-                < 25
-            ):
-                is_new: bool = False
-                break
-        if is_new:
-            queue_of_elements.append(color)
-    tracker_types[color] = current_contours
+                    is_new: bool = False
+                    break
+            if is_new:
+                queue_of_elements.append(color)
+        tracker_types[color] = current_contours
+
+    while True:
+        frame: numpy.ndarray = capture.read()[1]
+        hsv_frame: numpy.ndarray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        detector(red_mask.get_contours(hsv_frame), Colors.RED)
+        detector(green_mask.get_contours(hsv_frame), Colors.GREEN)
+        detector(orange_mask.get_contours(hsv_frame), Colors.ORANGE)
+        detector(white_mask.get_contours(hsv_frame), Colors.WHITE)
+
+        cv2.rectangle(frame, (area[0], area[2]), (area[1], area[3]), (0, 0, 0), 3)
+        # cv2.imshow("Frame", frame)
+        with lock:
+            outputFrame = frame.copy()
+        print(queue_of_elements)
+        if cv2.waitKey(delay) & 0xFF == ord("q"):
+            break
 
 
-while True:
-    frame: numpy.ndarray = capture.read()[1]
-    hsv_frame: numpy.ndarray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+def generate():
+    global lock, outputFrame
+    while True:
+        with lock:
+            if outputFrame is None:
+                continue
+            flag, encodedImage = cv2.imencode(".jpg", outputFrame)
+            if not flag:
+                continue
+        yield (
+            b"--frame\r\n"
+            b"Content-Type: image/jpeg\r\n\r\n" + bytearray(encodedImage) + b"\r\n"
+        )
 
-    detector(red_mask.get_contours(hsv_frame), Colors.RED)
-    detector(green_mask.get_contours(hsv_frame), Colors.GREEN)
-    detector(orange_mask.get_contours(hsv_frame), Colors.ORANGE)
-    detector(white_mask.get_contours(hsv_frame), Colors.WHITE)
 
-    cv2.rectangle(frame, (area[0], area[2]), (area[1], area[3]), (0, 0, 0), 3)
-    cv2.imshow("Frame", frame)
-    print(queue_of_elements)
-    if cv2.waitKey(delay) & 0xFF == ord("q"):
-        break
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/video_feed")
+def video_feed():
+    return Response(generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
+if __name__ == "__main__":
+    t1 = Thread(target=cv)
+    t1.daemon = True
+    t1.start()
+    app.run(host="0.0.0.0", threaded=True, port=5500)
